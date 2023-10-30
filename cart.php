b7920fefffcbba1b0c2051b109b8779681e25b9d
NicoDestephen <ndestephen@gmail.com>
2023-10-30 17:15:03 +0100

    add cookies in cart

--- cart.php
+++ cart.php
@@ -1,7 +1,17 @@
-<?php require 'inc/head.php'; ?>
+<?php 
+session_start();
+require 'inc/head.php'; ?>
 <section class="cookies container-fluid">
     <div class="row">
         TODO : Display shopping cart items from $_SESSION here.
+        <?php
+        foreach($_SESSION as $cookieName => $numberCookies) {
+            echo nl2br("\n") . $cookieName . " - " . $numberCookies  . nl2br("\n");
+        }
+        session_destroy();
+        unset($_SESSION);
+        
+        ?>
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
--- index.php
+++ index.php
@@ -1,8 +1,40 @@
-<?php require 'inc/data/products.php'; ?>
-<?php require 'inc/head.php'; ?>
+<?php 
+    session_start();
+    require 'inc/data/products.php';
+    require 'inc/head.php';
+   
+    if(isset($_GET['add_to_cart']) && !empty($_GET['add_to_cart'])) {
+        if($_GET['add_to_cart'] == 46) {
+                if(isset($_SESSION['Pecan nuts']) && $_SESSION['Pecan nuts'] >= 1) {
+                    $_SESSION['Pecan nuts'] += 1;
+                } else {
+                    $_SESSION['Pecan nuts'] = 1;
+                }
+        } elseif ($_GET['add_to_cart'] == 36) {
+                if(isset($_SESSION['Chocolate chips']) && $_SESSION['Chocolate chips'] >= 1) {
+                    $_SESSION['Chocolate chips'] += 1;
+                } else {
+                    $_SESSION['Chocolate chips'] = 1;
+                }
+        } elseif ($_GET['add_to_cart'] == 58) {
+                if(isset($_SESSION['Full chocolate cookie']) && $_SESSION['Full chocolate cookie'] >= 1) {
+                    $_SESSION['Full chocolate cookie'] += 1;
+                } else {
+                    $_SESSION['Full chocolate cookie'] = 1;
+                }
+        } else {
+                if(isset($_SESSION['M&M\'s cookies']) && $_SESSION['M&M\'s cookies'] >= 1) {
+                    $_SESSION['M&M\'s cookies'] += 1;
+                } else {
+                    $_SESSION['M&M\'s cookies'] = 1;
+                }
+        }
+    }    
+    ?>
 <section class="cookies container-fluid">
     <div class="row">
-        <?php foreach ($catalog as $id => $cookie) { ?>
+        <?php 
+            foreach ($catalog as $id => $cookie) { ?>
             <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                 <figure class="thumbnail text-center">
                     <img src="assets/img/product-<?= $id; ?>.jpg" alt="<?= $cookie['name']; ?>" class="img-responsive">
